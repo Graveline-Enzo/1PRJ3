109ba88d03eb19760380fbfbd76af781c37c3a55
Noe-Moreau <110332@ecole-it.com>
2026-03-17 16:09:26 +0100

    Add the basic structure

---
+++ inc/init.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+// -------------------- BDD
+// Connexion à la BDD
+$host = 'mysql:host=localhost;dbname=hair_it;';
+$login = 'root';
+$password = '';
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4'
+];
+
+try
+{
+    $pdo = new PDO($host, $login, $password, $options);
+}
+catch(PDOExeption $e)
+{
+    die("🔴Un problème est survenu lors de la tentative de connexion à la base données : " . $e->getMessage());
+}
+
+
+// -------------------- SESSION
+// Démmarage de la session
+session_start();
+
+// -------------------- CHEMIN
+// Création de la constante
+define("RACINE_SITE", "/1PRJ3/");
+
+// -------------------- VARIABLES
+// Initialisation de la variable contenue vide pour éviter les erreurs
+$contenu = '';
+
+// -------------------- AUTRES
+// Ici on inclus le fichier des fonctions
+require_once('fonction.inc.php');
+
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+require_once 'inc/init.inc.php';
+// ------- TRAITEMENT PHP
+
+// ------- AFFICHAGE HTML
+require_once 'inc/haut.inc.php';
+echo $contenu;
+require_once 'inc/bas.inc.php';
+?>
