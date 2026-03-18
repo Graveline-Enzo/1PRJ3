5e94e3fb38e784a957ab3d6044673086c32f5e5d
Noe-Moreau <110332@ecole-it.com>
2026-03-18 14:17:54 +0100

    Update header/footer

--- inc/bas.inc.php
+++ inc/bas.inc.php
@@ -1,10 +1,11 @@
 </div>
 </section>
 <footer>
-    <div class="container bg-dark text-white text-center py-2 position-absolute bottom-0 start-50 translate-middle-x">
-        <?= date('Y'); ?> - Tous droits réservés - hair-it.com
+    <div class="container bg-dark text-white text-center py-2">
+        <p class="mb-1">© <?= date('Y'); ?> <strong>Hair IT</strong> — Tous droits réservés.</p>
+        <p class="small">12 rue des Lilas, 75011 Paris • 01 23 45 67 89</p>
     </div>
 </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
--- inc/haut.inc.php
+++ inc/haut.inc.php
@@ -14,7 +14,7 @@
         <div class="container-fluid">
             
             <div class="logo-title">
-                <img src=".\ressource\scissors.svg" alt="logo scissors" class="header-logo-scissors">
+                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-scissors-icon lucide-scissors"><circle cx="6" cy="6" r="3"/><path d="M8.12 8.12 12 12"/><path d="M20 4 8.12 15.88"/><circle cx="6" cy="18" r="3"/><path d="M14.8 14.8 20 20"/></svg>
                 <a class="navbar-brand" href="#">Hair IT</a>
             </div>
 
